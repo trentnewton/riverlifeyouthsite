16cde79a83854c33fb1ef6dacc4266cb8af49613
Trent Newton <trent99878@gmail.com>
2017-04-11 13:20:17 +1000

    improved hide/show navigation

--- src/js/app.js
+++ src/js/app.js
@@ -87,41 +87,53 @@
 
 	// Hide / show the master navigation menu
 
-	var previousScroll = 0;
+	// var previousScroll = 0;
 
-	$(window).scroll(function (){
+	// $(window).scroll(function (){
 
-		var currentScroll = $(this).scrollTop();
+	// 	var currentScroll = $(this).scrollTop();
 
-		// If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
+	// 	// If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
 
-		if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+	// 	if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
 
-			// If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
+	// 		// If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
 
-			if (currentScroll > previousScroll){
-				window.setTimeout(hideNav, 300);
+	// 		if (currentScroll > previousScroll){
+	// 			window.setTimeout(hideNav, 300);
 
-			// Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
+	// 		// Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
 
-			} else {
-				window.setTimeout(showNav, 300);
-			}
+	// 		} else {
+	// 			window.setTimeout(showNav, 300);
+	// 		}
 
-			// Set the previous scroll value equal to the current scroll.
+	// 		// Set the previous scroll value equal to the current scroll.
 
-			previousScroll = currentScroll;
+	// 		previousScroll = currentScroll;
+	// 	}
+
+	// });
+
+	// function hideNav() {
+	// 	$('[data-nav-status="toggle"]').removeClass('is-visible').addClass('is-hidden');
+	// }
+
+	// function showNav() {
+	// 	$('[data-nav-status="toggle"]').removeClass('is-hidden').addClass('is-visible');
+	// }
+
+	var lastScrollTop = 0;
+
+	window.addEventListener("scroll", function () {
+		var st = window.pageYOffset || document.documentElement.scrollTop;
+		if (st > lastScrollTop){
+			document.getElementById("main-navigation").style.top = "-100%";
+		} else {
+			document.getElementById("main-navigation").style.top = "0";
 		}
-
-	});
-
-	function hideNav() {
-		$('[data-nav-status="toggle"]').removeClass('is-visible').addClass('is-hidden');
-	}
-
-	function showNav() {
-		$('[data-nav-status="toggle"]').removeClass('is-hidden').addClass('is-visible');
-	}
+		lastScrollTop = st;
+	}, false);
 
 	$(window).bind('load', function () {
 
