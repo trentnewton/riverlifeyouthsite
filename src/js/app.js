dd7122b75cc7ed7e3f8d969cb0b6597dab801ad8
Trent Newton <trent99878@gmail.com>
2017-04-23 13:53:48 +1000

    updated javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,61 @@
+// Hide / show the master navigation menu
+
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function () {
+	var st = window.pageYOffset || document.documentElement.scrollTop;
+	if (st > lastScrollTop){
+		document.getElementById('main-navigation').style.top = '-100%';
+	} else {
+		document.getElementById('main-navigation').style.top = '0';
+	}
+	lastScrollTop = st;
+}, false);
+
+// detect IE, returns version of IE or false, if browser is not Internet Explorer
+
+function detectIE() {
+	var ua = window.navigator.userAgent;
+
+	// Test values; Uncomment to check result …
+
+	// IE 10
+	// ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+	// IE 11
+	// ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+	// Edge 12 (Spartan)
+	// ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+	// Edge 13
+	// ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+	var msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+		// IE 10 or older => return version number
+		return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+	}
+
+	var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+		// IE 11 => return version number
+		var rv = ua.indexOf('rv:');
+		return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+	}
+
+	var edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+		// Edge (IE 12+) => return version number
+		return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+	}
+
+	// other browser
+	return false;
+}
+
+// jQuery functions
+
 (function ($){
 
 	'use strict';
@@ -85,56 +143,6 @@
 		}
 	});
 
-	// Hide / show the master navigation menu
-
-	// var previousScroll = 0;
-
-	// $(window).scroll(function (){
-
-	// 	var currentScroll = $(this).scrollTop();
-
-	// 	// If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
-
-	// 	if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
-
-	// 		// If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
-
-	// 		if (currentScroll > previousScroll){
-	// 			window.setTimeout(hideNav, 300);
-
-	// 		// Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
-
-	// 		} else {
-	// 			window.setTimeout(showNav, 300);
-	// 		}
-
-	// 		// Set the previous scroll value equal to the current scroll.
-
-	// 		previousScroll = currentScroll;
-	// 	}
-
-	// });
-
-	// function hideNav() {
-	// 	$('[data-nav-status="toggle"]').removeClass('is-visible').addClass('is-hidden');
-	// }
-
-	// function showNav() {
-	// 	$('[data-nav-status="toggle"]').removeClass('is-hidden').addClass('is-visible');
-	// }
-
-	var lastScrollTop = 0;
-
-	window.addEventListener('scroll', function () {
-		var st = window.pageYOffset || document.documentElement.scrollTop;
-		if (st > lastScrollTop){
-			document.getElementById('main-navigation').style.top = '-100%';
-		} else {
-			document.getElementById('main-navigation').style.top = '0';
-		}
-		lastScrollTop = st;
-	}, false);
-
 	$(window).bind('load', function () {
 
 		// add animation class when in viewport
@@ -164,51 +172,6 @@
 		$('.page-main').removeClass('slope-page');
 		$('.ministry .content').removeClass().addClass('content small-12 columns');
 		$('body').addClass('old-ie');
-	}
-
-	/**
-	* detect IE
-	* returns version of IE or false, if browser is not Internet Explorer
-	*/
-
-	function detectIE() {
-		var ua = window.navigator.userAgent;
-
-		// Test values; Uncomment to check result …
-
-		// IE 10
-		// ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
-
-		// IE 11
-		// ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
-
-		// Edge 12 (Spartan)
-		// ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
-
-		// Edge 13
-		// ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
-
-		var msie = ua.indexOf('MSIE ');
-			if (msie > 0) {
-			// IE 10 or older => return version number
-			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-		}
-
-		var trident = ua.indexOf('Trident/');
-			if (trident > 0) {
-			// IE 11 => return version number
-			var rv = ua.indexOf('rv:');
-			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-		}
-
-		var edge = ua.indexOf('Edge/');
-			if (edge > 0) {
-			// Edge (IE 12+) => return version number
-			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-		}
-
-		// other browser
-		return false;
 	}
 
 	// detect iOS and add class to body
